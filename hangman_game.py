6b7fc43dfea40b20b42d5c1742af62a8aac47505
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-10-14 15:33:15 +0500

    some important variables and empty game loop created for the time being

--- hangman_game.py
+++ hangman_game.py
@@ -2,6 +2,17 @@
 import random
 # create a list of fruits
 list_of_words = ["banana", "mango", "grapes", "greyfruit"]
-#selection of secret word from the 
-secret_word = 
+# selection of secret word from the list of words
+secret_word = random.choice(list_of_words)
+# length of secret_word
+secret_word_length = len(secret_word)
+# number of guesses
+chances = secret_word_length + 2
+# game_loop
+for i in range(chances):
+
+    # user guess an alphabet at a time 
+    user_guess = input("Enter an alphabet: ")
+    
+ 
 
