8cd10ba7b338fe8450496ee41720ec2c5e7ba8b0
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-10-14 15:19:01 +0500

    Hangman game file created

---
+++ hangman_game.py
@@ -0,0 +1,7 @@
+# import random module for the selection of secret word
+import random
+# create a list of fruits
+list_of_words = ["banana", "mango", "grapes", "greyfruit"]
+#selection of secret word from the 
+secret_word = 
+
---
+++ word_guessing_game.py
@@ -0,0 +1,4 @@
+import random
+
+list_of_words = ["lahore", "karachi", "multan", "rawalpindi"]
+secret_word = random.choice(list_of_words)
